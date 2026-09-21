448c642687cf104a74f597519996745829e5a28b
sama233 <97009482+casama233@users.noreply.github.com>
2026-09-21 23:16:54 +0800

    Remove duplicated storage routing imports and dead locks

--- runtime/BP/scripts/bedrock/bar-cabinet.js
+++ runtime/BP/scripts/bedrock/bar-cabinet.js
@@ -1,9 +1,9 @@
 import {world,system,BlockPermutation} from '@minecraft/server';
 import {NS,CABINET_TYPES,CABINET_POSITION,barCabinetItem,barCabinetClickedLeft,barCabinetPut,barCabinetTake,barCabinetPosition,barCabinetKey,barCabinetAnchor,parseBarCabinetAnchor,barCabinetVisualPose,emptyBarCabinet,BarCabinetStore} from '../core/bar-cabinet.js';
-import {FACING,POSITION,facingForYaw,facingVector,faceOffset} from '../core/furniture.js';
+import {FACING,POSITION,facingForYaw,facingVector} from '../core/furniture.js';
 import {check} from '../core/util.js';
 import {planInventory,commitInventory,isPlainIngredient} from '../core/inventory.js';
-import {makeStack,hand,inventory,handSnapshot,sameHand,canWrite,blockAt,plus,tell,safe,finishPlayerBreak,air} from './transactions.js';
+import {makeStack,hand,inventory,canWrite,blockAt,plus,tell,air} from './transactions.js';
 import {installStatefulStorageRoutes} from './stateful-storage-router.js';
 const HELPER=NS+':bar_cabinet_bottle_visual',ANCHOR=NS+':bar_cabinet_anchor',store=new BarCabinetStore(world),visuals=new Map();let cursor=0;
 export const barCabinetDiagnostics={placed:0,inserted:0,taken:0,recovered:0,spawned:0,orphans:0,duplicates:0,repairs:0,errors:[]};
